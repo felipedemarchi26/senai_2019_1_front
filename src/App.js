ed0c0e8d8dcc2709285b523da7c4ae74b72601b0
Disraeli Stubert <33765601+disraeli-stubert@users.noreply.github.com>
2019-06-11 22:15:41 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ComponenteExemplo from './components/componente-exemplo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+	  super(props);
+
+	  this.state = {
+		  texto: 'Texto do State',
+		  textoDigitado: "",
+		  textoDigitado2: ""
+	  };
+
+	  this.onClick = this.onClick.bind(this);
+	  this.onChange = this.onChange.bind(this);
+  }
+
+  onClick() {
+	this.setState({
+		texto: 'Texto Alterado'
+	});
+  }
+
+  onChange(e) {
+	  this.setState({
+		  textoDigitado: e.target.value
+	  });
+  }
+
+  render() {
+	  return (
+		<div>
+			<p>Parágrafo 1</p>
+			<p>Parágrafo 2</p>
+			<ComponenteExemplo />
+			<button onClick={this.onClick}>Teste Botão</button>
+			<p> {this.state.texto} </p>
+			<input type="text" onChange={this.onChange}/>
+			<p> {this.state.textoDigitado} </p>
+			<input type="text" onChange={this.onChange}/>
+			<p> {this.state.textoDigitado2} </p>
+		</div>
+	  );
+  }
 }
 
 export default App;
---
+++ src/components/componente-exemplo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponenteExemplo extends Component {
+  render() {
+	  return (
+		<div>
+			<h1>Meu Componente</h1>
+		</div>
+	  );
+  }
+}
+
+export default ComponenteExemplo;
